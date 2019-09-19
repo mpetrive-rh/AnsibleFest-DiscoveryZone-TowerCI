8adff7b640ba272acc22ecfdcaaf9605fb3177cd
Marc Petrivelli <mpetrive@redhat.com>
2019-09-18 20:35:07 -0400

    add molecule testing

--- tower-starter-pack/roles/nginx_base/molecule-dind.sh
+++ tower-starter-pack/roles/nginx_base/molecule-dind.sh
@@ -4,5 +4,5 @@ docker run --rm -it \
     -v "$(pwd)":/tmp/$(basename "${PWD}"):ro \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -w /tmp/$(basename "${PWD}") \
-    ansible/molecule:latest \
+    petro026/molecule-old:latest \
     sudo molecule --debug test
